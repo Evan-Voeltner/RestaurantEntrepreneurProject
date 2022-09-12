1818fa19e8ffda1c71c872fa68dfcddac8b8a79f
Evan Voeltner <evanvoeltner@gmail.com>
2022-09-12 11:24:23 -0500

    feat: place_order method now asks user for their order and logs it

--- franchise.py
+++ franchise.py
@@ -1,8 +1,14 @@
 from logger import file_logger
+from order_factory import OrderFactory
 
 class Franchise:
     def __init__(self):
         self.location_number = 0
 
     def place_order(self):
-        pass
+        print('What would you like to order?')
+        print('Type 1 for Pizza, 2 for Pasta, 3 for Salad')
+        user_input = input()
+        
+        desired_order = OrderFactory.create_order(user_input)
+        file_logger.log_transaction(desired_order, self.location_number)
--- order_factory.py
+++ order_factory.py
@@ -8,11 +8,11 @@ class OrderFactory:
 
     @staticmethod
     def create_order(desired_dish):
-        if desired_dish == 'salad':
-            return Salad()
-        elif desired_dish == 'pasta':
-            return Pasta()
-        elif desired_dish == 'pizza':
+        if desired_dish == '1':
             return Pizza()
+        elif desired_dish == '2':
+            return Pasta()
+        elif desired_dish == '3':
+            return Salad()
         else:
             print(f'{desired_dish} is not a dish')
