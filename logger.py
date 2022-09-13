15230585c5aa6f67a9b084e7a11add2e89ba92c2
Evan Voeltner <evanvoeltner@gmail.com>
2022-09-13 10:52:06 -0500

    feat: log file keeps track of the total across buisness days

--- logger.py
+++ logger.py
@@ -5,9 +5,17 @@ class Logger:
 
 
     def log_transaction(self, order, store_number):
+        log_file = open('log.txt', 'r')
+        last_log = log_file.read()
+        if last_log == '':
+            self.daily_sales = order.price
+        else: 
+            last_log = last_log.rsplit('$')
+            self.daily_sales = int(last_log[len(last_log)-1])
+            self.daily_sales += order.price
         self.transaction_count += 1
-        self.daily_sales += order.price
-        
+        log_file.close()
+
         log_string = f'{order.name} was purchased at location {store_number} for ${order.price} | Total: ${self.daily_sales} \n'
         
         log_file = open('log.txt', 'a')
